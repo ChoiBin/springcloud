a4a19e972daa8b0d9fa01879f0adccd8ab9790a5
choibin <841494724@qq.com>
2020-03-21 13:37:21 +0800

    注册中心学习

---
+++ cloud-consumerconsul-order80/src/main/java/com/choi/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,19 @@
+package com.choi.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by ChoiBin on 2020/3/20
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
